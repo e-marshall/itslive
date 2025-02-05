e6cd29c4fd1022542e05d4eee419ef284e243e04
e-marshall <em.marshall.108@gmail.com>
2025-02-04 21:38:33 -0700

    add script

--- intro.md
+++ intro.md
@@ -1,18 +1,18 @@
 # Introduction
 
-This tutorial will demonstrate how to access and work with with multi-dimensional remote sensing data using the python package and open source project `xarray`. This example will use a glacier surface velocity dataset called [ITS_LIVE](https://its-live.jpl.nasa.gov/). 
+This tutorial will demonstrate how to access and work with with multi-dimensional remote sensing data using the python package and open source project `xarray`. This example will use a glacier surface velocity dataset called [ITS_LIVE](https://its-live.jpl.nasa.gov/).
 
 ## Overview
 
-This tutorial contains a number of jupyter notebooks demonstrating various steps of a typical workflow for accessing, processing and analyzing remote sensing data. The structure is as follows:  
-&nbsp;&nbsp;&nbsp;&nbsp; 1. Data access (accessing ITS_LIVE data stored in s3 buckets on Amazon Web Services (AWS))  
-&nbsp;&nbsp;&nbsp;&nbsp; 2. Processing and analysis at the scale of an individual glacier  
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; a. Clipping large raster to a smaller area of interest and preliminary dataset inspection  
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; b. Using xarray for data analysis and visualization  
-&nbsp;&nbsp;&nbsp;&nbsp; 3. Processing and analysis of a group of glaciers within a sub-region  
+This tutorial contains a number of jupyter notebooks demonstrating various steps of a typical workflow for accessing, processing and analyzing remote sensing data. The structure is as follows:
+&nbsp;&nbsp;&nbsp;&nbsp; 1. Data access (accessing ITS_LIVE data stored in s3 buckets on Amazon Web Services (AWS))
+&nbsp;&nbsp;&nbsp;&nbsp; 2. Processing and analysis at the scale of an individual glacier
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; a. Clipping large raster to a smaller area of interest and preliminary dataset inspection
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; b. Using xarray for data analysis and visualization
+&nbsp;&nbsp;&nbsp;&nbsp; 3. Processing and analysis of a group of glaciers within a sub-region
 
 ## Learning objectives
-This tutorial demonstrates how to use xarray for scientific investigation of remote sensing data. The learning goals include **high-level science goals** as well as specific *python and xarray techniques*. 
+This tutorial demonstrates how to use xarray for scientific investigation of remote sensing data. The learning goals include **high-level science goals** as well as specific *python and xarray techniques*.
 
 * **Load ITS_LIVE data from AWS S3 buckets**
 *Lazily load cloud datasets using xarray, dask and zarr*
@@ -34,7 +34,7 @@ This tutorial demonstrates how to use xarray for scientific investigation of rem
 *Use's xarray's groupby and functionality*
 
 ## Tutorial structure
-Navigate to the other pages in this jupyter book to find out more about this tutorial. You can check out the data and open source python tools we'll be using before we get started with the notebook in the [Software and Data](software.ipynb) page. 
+Navigate to the other pages in this jupyter book to find out more about this tutorial. You can check out the data and open source python tools we'll be using before we get started with the notebook in the [Software and Data](software_and_data.ipynb) page.
 
 ```{tableofcontents}
 ```
--- itslivetools.py
+++ itslivetools.py
@@ -1,57 +1,95 @@
 import geopandas as gpd
-import os
-import numpy as np
+from shapely.geometry import Point, Polygon
+from typing import Union
 import xarray as xr
-import rioxarray as rxr
 
-from shapely.geometry import Polygon
-from shapely.geometry import Point
-import hvplot.pandas
-import hvplot.xarray
 
-import json
-import s3fs
 
-def read_in_s3(http_url, chunks = 'auto'):
+def read_in_s3(http_url: str, chunks_arg: Union[None, str, dict] = "auto") -> xr.Dataset:
+    """
+    Reads a zarr datacube given an S3 URL using
+    xarray and returns it as a xr.Dataset.
 
-    #s3_url = http_url.replace('http','s3')   <-- as of Fall 2023, can pass http urls directly to xr.open_dataset()
-    #s3_url = s3_url.replace('.s3.amazonaws.com','')
+    Parameters
+    ----------
+    http_url : str
+        The HTTP URL of the dataset to be read.
+    chunks : str or dict, optional
+        The chunk size for dask. Default is 'auto'.
+        If None, the dataset will be loaded without chunking.
 
-    datacube = xr.open_dataset(http_url, engine = 'zarr',
-                                #storage_options={'anon':True},
-                                chunks = chunks)
+    Returns
+    -------
+    xarray.Dataset
+        The dataset loaded from the given URL.
+    """
+
+    if chunks_arg is not None:
+
+        datacube = xr.open_dataset(
+            http_url,
+            engine="zarr",
+            # storage_options={'anon':True},
+            chunks=chunks_arg,
+        )
+    elif chunks_arg is None:
+
+        datacube = xr.open_dataset(http_url, engine="zarr")
 
     return datacube
 
-def get_bounds_polygon(input_xr):
-    
-    xmin = input_xr.coords['x'].data.min()
-    xmax = input_xr.coords['x'].data.max()
 
-    ymin = input_xr.coords['y'].data.min()
-    ymax = input_xr.coords['y'].data.max()
+def get_bounds_polygon(input_xr: xr.Dataset) -> gpd.GeoDataFrame:
+    """
+    Generate a GeoDataFrame containing a polygon that represents the bounding box of the input xarray Dataset.
+    Parameters
+    ----------
+    input_xr : xr.Dataset
+        An xarray Dataset containing 'x' and 'y' coordinates.
+    Returns
+    -------
+    gpd.GeoDataFrame
+        A GeoDataFrame with a single polygon geometry representing the bounding box of the input dataset.
+    """
 
-    pts_ls = [(xmin, ymin), (xmax, ymin),(xmax, ymax), (xmin, ymax), (xmin, ymin)]
+    xmin = input_xr.coords["x"].data.min()
+    xmax = input_xr.coords["x"].data.max()
+
+    ymin = input_xr.coords["y"].data.min()
+    ymax = input_xr.coords["y"].data.max()
+
+    pts_ls = [(xmin, ymin), (xmax, ymin), (xmax, ymax), (xmin, ymax), (xmin, ymin)]
 
     crs = f"epsg:{input_xr.mapping.spatial_epsg}"
 
     polygon_geom = Polygon(pts_ls)
-    polygon = gpd.GeoDataFrame(index=[0], crs=crs, geometry=[polygon_geom]) 
-    
-    return polygon
+    polygon_gdf = gpd.GeoDataFrame(index=[0], crs=crs, geometry=[polygon_geom])
 
-def find_granule_by_point(input_point):
-    '''returns url for the granule (zarr datacube) containing a specified point. point must be passed in epsg:4326
-    '''
-    catalog = gpd.read_file('https://its-live-data.s3.amazonaws.com/datacubes/catalog_v02.json')
+    return polygon_gdf
 
-    #make shapely point of input point
-    p = gpd.GeoSeries([Point(input_point[0], input_point[1])],crs='EPSG:4326')
-    #make gdf of point
-    gdf = gdf = gpd.GeoDataFrame({'label': 'point', 
-                                  'geometry':p})
-    #find row of granule 
-    granule = catalog.sjoin(gdf, how='inner')
 
-    url = granule['zarr_url'].values[0]
+def find_granule_by_point(input_point: list) -> str:
+    """
+    Returns the URL for the granule (zarr datacube) containing a specified point.
+
+    Parameters
+    ----------
+    input_point : list
+        A list containing the [lon,lat] coordinates of the point in EPSG:4326 format.
+
+    Returns
+    -------
+    str
+        The URL of the granule containing the specified point.
+    """
+    catalog = gpd.read_file("https://its-live-data.s3.amazonaws.com/datacubes/catalog_v02.json")
+
+    # make shapely point of input point
+    p = gpd.GeoSeries([Point(input_point[0], input_point[1])], crs="EPSG:4326")
+    # make gdf of point
+    gdf = gdf = gpd.GeoDataFrame({"label": "point", "geometry": p})
+    # find row of granule
+    granule = catalog.sjoin(gdf, how="inner")
+
+    url = granule["zarr_url"].values[0]
     return url
